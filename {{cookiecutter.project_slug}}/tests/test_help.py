45480335d53123bb0684e7b9a6a3118708e0bc0d
Davide Nastri <d.nastri@gmail.com>
2022-09-13 14:54:17 +0200

    Update test_help.py

--- {{cookiecutter.project_slug}}/tests/test_help.py
+++ {{cookiecutter.project_slug}}/tests/test_help.py
@@ -4,6 +4,6 @@ from {{cookiecutter.project_slug}} import app
 
 def test_help():
     runner = CliRunner()
-    result = runner.invoke(app.cli)
+    result = runner.invoke(app.{{cookiecutter.cli_command}})
     assert result.exit_code == 0
     assert "Usage: " in result.output
